6a4db77753659bc87dd2f71bf73a645cc1d91442
bribouezv <bribou@easyvista.com>
2025-07-24 14:54:13 +0200

    Remove templateFile

--- main.tf
+++ main.tf
@@ -1,36 +1,36 @@
+locals {
+  haproxy_config = templatefile("${path.module}/templates/haproxy.cfg.tpl", {
+    backend_ips = var.haproxy_backend_ips
+    https_port  = var.haproxy_https_nodeport
+  })
+}
+
 resource "oci_core_instance" "haproxy" {
   availability_domain = var.availability_domain
   compartment_id      = var.compartment_id
   shape               = var.shape
 
+  display_name = "haproxy-instance"
+
   create_vnic_details {
-    subnet_id = var.subnet_id
-    assign_public_ip = true
+    subnet_id         = var.subnet_id
+    assign_public_ip  = true
   }
 
   source_details {
     source_type = "image"
-    source_id = data.oci_core_images.ubuntu_latest.images[0].id
+    source_id   = data.oci_core_images.ubuntu_latest.images[0].id
   }
 
   metadata = {
     ssh_authorized_keys = var.ssh_public_key
-    user_data           = base64encode(data.template_file.cloud_init.rendered)
+    user_data           = base64encode(templatefile("${path.module}/cloud-init/haproxy.yml", {
+      haproxy_config = local.haproxy_config
+    }))
   }
-}
 
-data "template_file" "haproxy_config" {
-  template = file("${path.module}/templates/haproxy.cfg.tpl")
-  vars = {
-    backend_ips = var.haproxy_backend_ips
-    https_port  = var.haproxy_https_nodeport
-  }
-}
-
-data "template_file" "cloud_init" {
-  template = file("${path.module}/cloud-init/haproxy.yml")
-  vars = {
-    haproxy_config = data.template_file.haproxy_config.rendered
+  defined_tags = {
+    "Oracle-Tags.CreatedBy" = "benjamin"
   }
 }
 
