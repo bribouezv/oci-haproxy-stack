9df33d53c1e6a82dcb47f3a1c464b09a832d8bda
bribouezv <bribou@easyvista.com>
2025-07-24 12:08:15 +0200

    Try to fix bugs backend ips

--- variables.tf
+++ variables.tf
@@ -6,8 +6,9 @@ default = "VM.Standard.E2.1.Micro"
 }
 variable "ssh_public_key" {}
 variable "haproxy_backend_ips" {
-  type        = string
-  description = "List of backend IP addresses in the form of a comma-separated string (for example, 10.1.0.10,10.1.0.11)"
+  type        = list(string)
+  description = "List of backend IP addresses"
+  default = ["0.0.0.0/0"]
 }
 variable "haproxy_https_nodeport" {
   type        = number
