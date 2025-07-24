27c9dd9741b6588aac19accd5f5ad382084b05ca
bribouezv <bribou@easyvista.com>
2025-07-24 11:34:54 +0200

    Edit variables.tf to fill nodeport https

--- variables.tf
+++ variables.tf
@@ -8,10 +8,9 @@ variable "ssh_public_key" {}
 variable "haproxy_backend_ips" {
   description = "Liste des IPs vers lesquelles le HAProxy redirigera le trafic (nodes du cluster OKE)"
   type        = list(string)
+  default = []
 }
-variable "haproxy_node_ports" {
-  description = "NodePort HTTP et HTTPS exposés par l'ingress controller"
-  type = object({
-    https = number
-  })
+variable "haproxy_https_nodeport" {
+  type        = number
+  description = "NodePort HTTPS exposé par l'ingress controller"
 }
