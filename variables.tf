536912d40fd73c53216bdba9bb57ed7c5989cc73
bribouezv <bribou@easyvista.com>
2025-07-24 11:59:01 +0200

    Try to fix bugs backend ips

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "shape" {
 default = "VM.Standard.E2.1.Micro"
 }
 variable "ssh_public_key" {}
-variable "haproxy_backend_ips_raw" {
+variable "haproxy_backend_ips" {
   type        = string
   description = "List of backend IP addresses in the form of a comma-separated string (for example, 10.1.0.10,10.1.0.11)"
 }
